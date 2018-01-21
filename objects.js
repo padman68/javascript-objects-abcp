d9c8d8e47554e56fc2368fc671d4f6106b5e6d62
Shayne Paddock <shayne@nerdystuff.ca>
2018-01-21 15:30:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,5 @@
 var playlist = new Object ({ artistName: 'songTitle' });
 //var playlist = {'song':'title'};
-var playlist = {sugar:'candy'}
 playlist.artistName = 'Bobby';
 
 function updatePlaylist(playlist, artistName, songTitle) {
